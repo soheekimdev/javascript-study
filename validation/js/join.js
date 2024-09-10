e60bd7d18f0d277eb1d2be46804e43f5d136cead
soheekim <soheekim.dev@gmail.com>
2024-09-10 18:47:30 +0900

    refactor: 유효성 검사 로직 리팩토링
    - fieldList 배열에 각 form field의 정보를 저장하고 관리할 수 있도록 변경
    - 모든 field에 대해 재사용 가능한 validCheck 함수 추가
    - DOM 요소 선택 과정에서의 코드 간소화
    - 이메일 유효성 검사 정규식 추가
    - forEach 루프를 사용하여 이벤트 리스너 간소화

--- validation/js/join.js
+++ validation/js/join.js
@@ -16,6 +16,7 @@ const letterCheck = {
   checkNum: /[0-9]/,
   checkSpc: /[~!@#$%^&*()_+|<>?:{}]/,
   checkKor: /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/,
+  checkEmail: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
   // TODO: 일부 특수문자만 허용하려면?
 };
 
@@ -23,86 +24,95 @@ const letterCheck = {
 const form = document.getElementById('form');
 const btnSubmit = document.getElementById('btnSubmit');
 
-// TODO: form field 객체 선택 과정을 간소화할 방법?
-const userId = form.id;
-const userPw1 = form.pw1;
-const userPw2 = form.pw2;
-const userName = form.name;
-const userPhone = form.phone;
-const userPosition = form.position;
-const userGender = form.gender;
-const userEmail = form.email;
-const userIntro = form.intro;
+// ------ form field 객체 리스트 ------
+const fieldList = [
+  {
+    name: 'id',
+    condition: (value) =>
+      value.length >= 5 &&
+      value.length <= 13 &&
+      letterCheck.checkEnga.test(value) &&
+      !letterCheck.checkEngA.test(value) &&
+      !letterCheck.checkKor.test(value) &&
+      !letterCheck.checkSpc.test(value),
+    errMsg: '5~13자의 영문 소문자, 숫자를 입력하세요.',
+  },
+  {
+    name: 'pw1',
+    condition: (value) =>
+      value.length >= 8 &&
+      value.length <= 30 &&
+      letterCheck.checkEngAll.test(value) &&
+      letterCheck.checkNum.test(value) &&
+      letterCheck.checkSpc.test(value),
+    errMsg: '8~30자의 영문, 숫자, 특수문자를 입력하세요.',
+  },
+  {
+    name: 'pw2',
+    condition: (value, form) => value === form.elements['pw1'].value,
+    errMsg: '동일한 비밀번호를 입력하세요.',
+  },
+  {
+    name: 'name',
+    condition: (value) => value.trim() !== '',
+    errMsg: '이름을 입력하세요.',
+  },
+  {
+    name: 'phone',
+    condition: (value) => value.trim() !== '',
+    errMsg: '휴대폰 번호를 입력하세요.',
+  },
+  {
+    name: 'position',
+    condition: (value) => value.trim() !== '',
+    errMsg: '원하는 직무를 선택하세요.',
+  },
+  // {
+  //   name: 'gender',
+  //   condition: (value) =>
+  //     value.trim() !== ''
+  //   ,
+  //   errMsg: '성별을 선택하세요.',
+  // },
+  {
+    name: 'email',
+    condition: (value) => letterCheck.checkEmail.test(value),
+    errMsg: '유효한 이메일 주소를 입력하세요.',
+  },
+  {
+    name: 'intro',
+    condition: (value) => value.trim() !== '',
+    errMsg: '자기소개를 입력하세요.',
+  },
+];
 
 // ------ 유효성 검사 ------
 let isValid = false;
 
-// TODO: 각 필드 별 유효성 검사 반복 코드 간소화
-// 아이디 유효성 검사
-// 조건: 5~13자 AND 영문 소문자 (+ 숫자) (대문자 영어, 한글 및 특수문자 입력 불가능)
-userId.addEventListener('blur', function () {
-  const value = this.value;
-  const errMsg = document.getElementById(userId.id + 'Err');
+// 유효성 검사 함수
+const validCheck = (field, form) => {
+  const el = form.elements[field.name];
+  const value = el.value;
+  const errMsgEl = document.getElementById(el.id + 'Err');
 
-  if (
-    value.length >= 5 &&
-    value.length <= 13 &&
-    letterCheck.checkEnga.test(value) &&
-    !letterCheck.checkEngA.test(value) &&
-    !letterCheck.checkKor.test(value) &&
-    !letterCheck.checkSpc.test(value)
-  ) {
-    isValid = true;
-    errMsg.style.display = 'none';
+  if (field.condition(value, form)) {
+    errMsgEl.style.display = 'none';
+    return true;
   } else {
-    isValid = false;
-    errMsg.innerHTML = '5~13자의 영문 소문자, 숫자를 입력해주세요.';
-    errMsg.style.display = 'block';
+    errMsgEl.innerHTML = field.errMsg;
+    errMsgEl.style.display = 'block';
+    return false;
   }
+};
+
+// 각 필드에 대해 유효성 검사 진행
+fieldList.forEach((field) => {
+  const el = form.elements[field.name];
+  el.addEventListener('blur', () => validCheck(field, form));
 });
-
-// 비밀번호 유효성 검사
-// 조건: 8~30자 AND 영문 AND 숫자 AND 특수문자
-userPw1.addEventListener('blur', function () {
-  const value = this.value;
-  const errMsg = document.getElementById(userPw1.id + 'Err');
-
-  if (
-    value.length >= 8 &&
-    value.length <= 30 &&
-    letterCheck.checkEngAll.test(value) &&
-    letterCheck.checkNum.test(value) &&
-    letterCheck.checkSpc.test(value)
-  ) {
-    isValid = true;
-    errMsg.style.display = 'none';
-  } else {
-    isValid = false;
-    errMsg.innerHTML = '8~30자의 영문, 숫자, 특수문자를 입력해주세요.';
-    errMsg.style.display = 'block';
-  }
-});
-
-// 비밀번호 확인 유효성 검사
-// 조건: 비밀번호 입력값과 동일
-userPw2.addEventListener('blur', function () {
-  const value = this.value;
-  const errMsg = document.getElementById(userPw2.id + 'Err');
-
-  if (value === userPw1.value) {
-    isValid = true;
-    errMsg.style.display = 'none';
-  } else {
-    isValid = false;
-    errMsg.innerHTML = '동일한 비밀번호를 입력해주세요.';
-    errMsg.style.display = 'block';
-  }
-});
-
-console.log(isValid);
 
 // ------ 폼 제출 이벤트 핸들러 ------
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  //
+  // 작업 중
 });

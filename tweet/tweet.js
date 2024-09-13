e879a8f269cbc96e86390435be42f5d1456a7630
soheekim <soheekim.dev@gmail.com>
2024-09-13 16:51:35 +0900

    feat: 좋아요 카운트 핸들링 이벤트, 함수 추가 및 함수명 변경

--- tweet/tweet.js
+++ tweet/tweet.js
@@ -13,9 +13,11 @@
 const tweetButton = document.querySelector('#postTweet'); // 트윗 게시 버튼 요소
 const tweetInput = document.querySelector('#tweetInput'); // 트윗을 입력할 input 요소
 const tweetsContainer = document.querySelector('#tweets_container'); // 트윗이 게시될 컨테이너
+const likeButton = document.querySelector('.like-button');
+const likeCount = document.querySelector('.like-counter');
 
-// 입력값 유무에 따른 버튼 비활성화 여부 처리
-const setTweetButtonState = () => {
+// 입력값 유무에 따른 '게시' 버튼 비활성화 여부 처리
+const handleTweetButtonState = () => {
   tweetInput.value.trim() ? (tweetButton.disabled = false) : (tweetButton.disabled = true);
 };
 
@@ -25,14 +27,20 @@ const createTweetElement = (content) => {
   tweet.classList.add('tweet');
 
   const tweetContent = document.createElement('p');
+  tweetContent.classList.add('tweet-text');
   tweetContent.textContent = content;
 
   const tweetLikeButton = document.createElement('button');
   tweetLikeButton.classList.add('like-button');
   tweetLikeButton.textContent = '♥';
 
+  const tweetLikeCount = document.createElement('p');
+  tweetLikeCount.classList.add('like-counter');
+  tweetLikeCount.textContent = 0;
+
   tweet.appendChild(tweetContent);
   tweet.appendChild(tweetLikeButton);
+  tweet.appendChild(tweetLikeCount);
 
   return tweet;
 };
@@ -43,11 +51,23 @@ const addTweet = () => {
   const tweet = createTweetElement(inputValue);
   tweetsContainer.appendChild(tweet);
   tweetInput.value = ''; // 사용자 입력 값 초기화
-  setTweetButtonState();
+  handleTweetButtonState();
+};
+
+// 좋아요 카운트 핸들링 함수
+const handleLikeCount = (event) => {
+  if (event.target.classList.contains('like-button')) {
+    const likeCountEl = event.target.nextSibling;
+    const likeCount = parseInt(likeCountEl.textContent);
+    likeCountEl.textContent = likeCount + 1;
+  }
 };
 
 // 트윗 게시 버튼 이벤트 핸들링
 tweetButton.addEventListener('click', addTweet);
 
 // 입력 값 변경 시 버튼 상태 업데이트
-tweetInput.addEventListener('input', setTweetButtonState);
+tweetInput.addEventListener('input', handleTweetButtonState);
+
+// 좋아요 카운트 핸들링
+tweetsContainer.addEventListener('click', handleLikeCount);

b58605b8a8e55ec89a4f849c1d441f2e532ed34d
soheekimdev <soheekim.dev@gmail.com>
2024-09-28 22:25:14 +0900

    feat: 기본적인 숫자 계산 기능 구현

--- oz-team-project-calculator/script.js
+++ oz-team-project-calculator/script.js
@@ -2,39 +2,67 @@
 
 const displayInput = document.querySelector('.calculator__result-input');
 const buttons = document.querySelectorAll('.calculator__button');
+let firstOperand = null;
+let secondOperand = null;
+let operator = null;
+let isNewInput = false;
 
-const displayButtonNumber = (event) => {
-  const buttonEl = event.target;
-  if (displayInput.value === '0') {
+const calculate = (firstOperand, operator, secondOperand) => {
+  switch (operator) {
+    case '/':
+      return parseInt(firstOperand) / parseInt(secondOperand);
+    case '*':
+      return parseInt(firstOperand) * parseInt(secondOperand);
+    case '-':
+      return parseInt(firstOperand) - parseInt(secondOperand);
+    case '+':
+      return parseInt(firstOperand) + parseInt(secondOperand);
+    default:
+      throw new Error('알 수 없는 연산자');
+  }
+};
+
+const handleOperator = (buttonEl) => {
+  firstOperand = displayInput.value;
+  operator = buttonEl.textContent;
+  isNewInput = true;
+};
+
+const handleNumber = (buttonEl) => {
+  if (displayInput.value === '0' || isNewInput) {
     displayInput.value = buttonEl.textContent;
   } else {
     displayInput.value += buttonEl.textContent;
   }
+  isNewInput = false;
 };
 
-const displayButtonPoint = (event) => {
-  const buttonEl = event.target;
+const handlePoint = (buttonEl) => {
   if (!displayInput.value.includes('.')) {
     displayInput.value += buttonEl.textContent;
   }
 };
 
-const displayButtonClear = () => {
+const clear = () => {
   displayInput.value = '0';
 };
 
-const displayButtonContent = (event) => {
+const handleButtonClick = (event) => {
   const buttonEl = event.target;
-  console.log(buttonEl.textContent);
+
   if (buttonEl.classList.contains('calculator__button--number')) {
-    displayButtonNumber(event);
+    handleNumber(buttonEl);
+  } else if (buttonEl.classList.contains('calculator__button--operator')) {
+    handleOperator(buttonEl);
+  } else if (buttonEl.textContent === '=') {
+    displayInput.value = calculate(firstOperand, operator, displayInput.value);
   } else if (buttonEl.textContent === '.') {
-    displayButtonPoint(event);
+    handlePoint(buttonEl);
   } else if (buttonEl.textContent === 'C') {
-    displayButtonClear(event);
+    clear();
   }
 };
 
 buttons.forEach((button) => {
-  button.addEventListener('click', displayButtonContent);
+  button.addEventListener('click', handleButtonClick);
 });
